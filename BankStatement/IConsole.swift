165adb106af8fdc0d98c6e26405470018e107efe
Fragkiskos <fragkiskos/katsimpas@comparethemarket.com>
2019-07-26 16:42:42 +0100

    remove comments

--- BankStatement/IConsole.swift
+++ BankStatement/IConsole.swift
@@ -1,11 +1,3 @@
-//
-//  IConsole.swift
-//  BankStatement
-//
-//  Created by Fragkiskos Katsimpas on 27/04/2019.
-//  Copyright © 2019 Fragkiskos Katsimpas. All rights reserved.
-//
-
 import Foundation
 
 protocol IConsole {
